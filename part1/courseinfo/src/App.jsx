6f52dcdc4a7f3b940b4304e0c6913f004820e969
Nate Webb <145239754+njw1520@users.noreply.github.com>
2024-06-04 20:19:42 -0400

    Refactored Header and Content components to call array from props instead of passing each individual value from App

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -18,19 +18,8 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content
-        p1={parts[0].name}
-        e1={parts[0].exercises}
-        p2={parts[1].name}
-        e2={parts[1].exercises}
-        p3={parts[2].name}
-        e3={parts[2].exercises}
-      />
-      <Total
-        e1={parts[0].exercises}
-        e2={parts[1].exercises}
-        e3={parts[2].exercises}
-      />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
@@ -46,9 +35,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.p1} exercises={props.e1} />
-      <Part part={props.p2} exercises={props.e2} />
-      <Part part={props.p3} exercises={props.e3} />
+      <Part part={props.parts[0].name} exercisee={props.parts[0].exercises} />
+      <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
     </div>
   );
 };
@@ -66,7 +55,12 @@ const Part = (props) => {
 const Total = (props) => {
   return (
     <div>
-      <p>Number of exercises: {props.e1 + props.e2 + props.e3}</p>
+      <p>
+        Number of exercises:{" "}
+        {props.parts[0].exercises +
+          props.parts[1].exercises +
+          props.parts[2].exercises}
+      </p>
     </div>
   );
 };
